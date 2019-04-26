8cb41084343d36568c8fbee7e6d5d5d9a311800a
eemelitn <eemeli.niemi@gmail.com>
2019-04-26 23:47:39 +0300

    urheilu/activity

--- app/src/main/java/com/example/healttecapp/MainUrheiluActivity.java
+++ app/src/main/java/com/example/healttecapp/MainUrheiluActivity.java
@@ -2,6 +2,8 @@ package com.example.healttecapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainUrheiluActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainUrheiluActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_urheilu);
+
+        SeekBar seekBar = findViewById(R.id.SeekBarUrheilu);
+        final TextView textViewProgress = findViewById(R.id.textViewProgress);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textViewProgress.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
