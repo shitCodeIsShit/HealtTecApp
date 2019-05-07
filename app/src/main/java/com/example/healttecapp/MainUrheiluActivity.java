ebd5b771fb33a60026665a3d36742d7ca8ff22ca
vvolvverine <jjjhjelt@gmail.com>
2019-05-07 20:06:27 +0300

    jatkuu

--- app/src/main/java/com/example/healttecapp/MainUrheiluActivity.java
+++ app/src/main/java/com/example/healttecapp/MainUrheiluActivity.java
@@ -1,15 +1,12 @@
 package com.example.healttecapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-
-import static com.example.healttecapp.ActivityDatabaseAdapter.*;
-import static com.example.healttecapp.R.id.textViewUrheiluArvo;
 
 
 public class MainUrheiluActivity extends AppCompatActivity {
