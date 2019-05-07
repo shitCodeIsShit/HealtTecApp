cc2f8db5b06cb9c3c317388d26f52360a3e99bf7
vvolvverine <jjjhjelt@gmail.com>
2019-05-07 22:05:48 +0300

    jatkuu

--- app/src/main/java/com/example/healttecapp/AjanValitsin.java
+++ app/src/main/java/com/example/healttecapp/AjanValitsin.java
@@ -8,10 +8,19 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import java.util.Calendar;
 
+/**
+ * @author Jimi Hjelt
+ */
 
 public class AjanValitsin extends DialogFragment {
+    /** Luodaan kellodialogi, josta voidaan valita tunnit ja minuutit.
+     *
+     * @param savedInstanceState Palauttaa aiemman tilan kellonajasta.
+     * @return Palauttaa kellonajan valitsimen, jonka aika ensimmäisellä avauksella on sama kuin puhelimen kellon ja tuntijärjestelmä määräytyy puhelimen asetusten mukaan (24h vai 12h)
+     */
     @NonNull
     @Override
+
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Calendar c = Calendar.getInstance();
         int tunti = c.get(Calendar.HOUR_OF_DAY);
--- app/src/main/java/com/example/healttecapp/MainActivity.java
+++ app/src/main/java/com/example/healttecapp/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+/**@author Jimi Hjelt
+ *
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private Button button4;
     private Button button5;
 
+    /** Käynnistäessä asetetaan oikea layout, luodaan database, valitaan oikea maskotti pistelaskurin määrän mukaan ja napeille määritetään toiminnot mitkä tapahtuu klikkauksesta.
+     *
+     * @param savedInstanceState Aiempi tila
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
 
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
+            /** uni painiketta painettaessa avautuu ajanvalitsinaktiviteetti näkymä
+             *
+             * @param v button2 näkymä
+             */
             @Override
             public void onClick(View v) {
                 openKello();
@@ -38,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
         button3 = findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener() {
+            /** urheilu painiketta painettaessa avutuu urheiluaktiviteetti näkymä
+             *
+             * @param v button3 näkymä
+             */
             @Override
             public void onClick(View v) {
                 openUrheiluActivity();
@@ -45,6 +60,10 @@ public class MainActivity extends AppCompatActivity {
         });
         button4 = findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
+            /** ruokailu painiketta painettaessa avautuu ruokailuaktiviteetti näkymä
+             *
+             * @param v button4 näkymä
+             */
             @Override
             public void onClick(View v) {
                 openRuokailu();
@@ -52,6 +71,10 @@ public class MainActivity extends AppCompatActivity {
         });
         button5 = findViewById(R.id.button5);
         button5.setOnClickListener(new View.OnClickListener() {
+            /** tiedot painiketta painettaessa avautuu tietoaktiviteetti näkymä
+             *
+             * @param v button5 näkymä
+             */
             @Override
             public void onClick(View v) {
                 openTiedot();
@@ -60,21 +83,31 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    /** Metodi jolla aloitetaan uniaktiviteetti
+     *
+     */
     public void openKello() {
         Intent intentKello = new Intent(this, MainKelloActivity.class);
         startActivity(intentKello);
     }
-
+    /** Metodi jolla aloitetaan urheiluaktiviteetti
+     *
+     */
     public void openUrheiluActivity() {
         Intent intentUrheilu = new Intent(this, MainUrheiluActivity.class);
         startActivity(intentUrheilu);
     }
-
+    /** Metodi jolla aloitetaan ruokailuaktiviteetti
+     *
+     */
     public void openRuokailu(){
         Intent intentRuokailu = new Intent (this, AddFoodMenyActivity.class);
         startActivity(intentRuokailu);
     }
-
+    /** Metodi jolla aloitetaan tietoaktiviteetti
+     *
+     */
     public void openTiedot(){
         Intent intentTiedot = new Intent(this, TiedotActivity.class);
         startActivity(intentTiedot);
